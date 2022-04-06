7db009348adc9a3c00170ec21566ea5fb8f556ad
Guy <54724504+Willphel@users.noreply.github.com>
2022-04-05 21:45:51 -0400

    update

--- FreqDetec/FreqDetec.ino
+++ FreqDetec/FreqDetec.ino
@@ -13,55 +13,59 @@ unsigned long microSeconds;
 const int buttonPin = 2;
 double vReal[SAMPLES]; 
 double vImag[SAMPLES]; 
-char notes[180];
+char notes[50];
+int notetime[50];
 int numNotes = 0;
+int deltatime = 0;
+unsigned long recent = 0;
+float breaktime = 700;
 void playNote(char Note){
 
   switch(Note){
     case 'C':
-      waist.write(0);
+      waist.write(45);
       delay(50);
       lowerLeft();
       delay(150);
       break;
     case 'D':
-      waist.write(15);
+      waist.write(30);
       delay(50);
       lowerLeft();
       delay(150);
       break;
     case 'E':
-      waist.write(30);
+      waist.write(22);
       delay(50);
       lowerLeft();
       delay(150);
       break;
     case 'F':
-      waist.write(45);
+      waist.write(10);
       delay(50);
       lowerLeft();
       delay(150);
       break;
     case 'G':
-      waist.write(0);
+      waist.write(35);
       delay(50);
       lowerRight();
       delay(150);
       break;
     case 'A':
-      waist.write(15);
+      waist.write(22);
       delay(50);
       lowerRight();
       delay(150);
       break;
     case 'B':
-      waist.write(30);
+      waist.write(10);
       delay(50);
       lowerRight();
       delay(150);
       break;
     case 'H':
-      waist.write(45);
+      waist.write(0);
       delay(50);
       lowerRight();
       delay(150);
@@ -88,23 +92,20 @@ void setup()
     leftArm.attach(3);
     rightArm.attach(6);
 
-    waist.write(0);
-    leftArm.write(0);
-    rightArm.write(0);
+   leftArm.write(0);
+    rightArm.write(180);
     delay(5000);
-    //waist.write(50);
+    waist.write(30);
 }
  
 void loop() 
 {  
   int buttonState = digitalRead(buttonPin);
   if(buttonState==HIGH){
-    delay(3000);
-    buttonState = digitalRead(buttonPin);
+    delay(2000);
     Serial.println("Recording");
-    //buttonState = LOW;
-    while(numNotes <180 && buttonState == HIGH){
-        /*Sample SAMPLES times*/
+    while(numNotes <50 && buttonState == HIGH){
+       
     for(int i=0; i<SAMPLES; i++)
     {
         microSeconds = micros();  
@@ -128,69 +129,109 @@ void loop()
     
     
     if(peak > 1090 && peak < 2070){
-      //Serial.print("");
-      //Serial.println(deltatime);
-      //if(deltatime < 4);
-      if(1090 < peak && peak < 1108){
-        Serial.println("C");
-        notes[numNotes] = 'C';
-        numNotes++;
-        //timer = newtimer;
-        delay(500);
+      deltatime = millis() - recent;
+      
+      if(deltatime > breaktime){
+        float temp = peak*(SAMPLES/SAMPLING_FREQUENCY);
+        if(1090 < peak && peak < 1108){
+          Serial.println("C");
+          Serial.println(vReal[(int)floor(temp)]);
+          notes[numNotes] = 'C';
+          notetime[numNotes] = deltatime;
+          numNotes++;
+          recent = millis();
+          Serial.println(deltatime);
+          //delay(500);
+        }
+        else if(peak > 1210 && peak < 1224){
+          Serial.println("D");
+          Serial.println(vReal[(int)floor(temp)]);
+          notes[numNotes] = 'D';
+          notetime[numNotes] = deltatime;
+          numNotes++;
+          //delay(250);
+          recent = millis();
+          Serial.println(deltatime);
+        }
+        else if( 1340 < peak && peak < 1370){
+          Serial.println("E");
+          Serial.println(vReal[(int)floor(temp)]);
+          notes[numNotes] = 'E';
+          notetime[numNotes] = deltatime;
+          numNotes++;
+          //delay(250);
+          recent = millis();
+          Serial.println(deltatime);
+        }
+        else if(1440 < peak && peak < 1460){
+          if(deltatime>breaktime*1.2){
+            Serial.println("F");
+            Serial.println(vReal[(int)floor(temp)]);
+            notes[numNotes] = 'F';
+            notetime[numNotes] = deltatime;
+            numNotes++;
+            //delay(250);
+            recent = millis();
+            Serial.println(deltatime);
+          }
+        }
+        else if(1620 < peak && peak < 1630){
+          Serial.println("G");
+          Serial.println(vReal[(int)floor(temp)]);
+          notes[numNotes] = 'G';
+          notetime[numNotes] = deltatime;
+          numNotes++;
+          //delay(250);
+          recent = millis();
+          Serial.println(deltatime);
+        }
+        else if(1820 < peak && peak < 1840){
+          Serial.println("A");
+          Serial.println(vReal[(int)floor(temp)]);
+          notes[numNotes] = 'A';
+          notetime[numNotes] = deltatime;
+          numNotes++;
+          //delay(250);
+          recent = millis();
+          Serial.println(deltatime);
+        }
+        else if(2020 < peak && peak < 2045){
+          Serial.println("B");
+          Serial.println(vReal[(int)floor(temp)]);
+          notes[numNotes] = 'B';
+          notetime[numNotes] = deltatime;
+          numNotes++;
+          //delay(250);
+          recent = millis();
+          Serial.println(deltatime);
+        }
+        else if(2050 < peak && peak < 2070){
+          Serial.println("C");
+          Serial.println(vReal[(int)floor(temp)]);
+          notes[numNotes] = 'H';
+          notetime[numNotes] = deltatime;
+          numNotes++;
+          //delay(250);
+          recent = millis();
+          Serial.println(deltatime);
+         }
+        else;
       }
-      else if(peak > 1210 && peak < 1224){
-        Serial.println("D");
-        notes[numNotes] = 'D';
-        numNotes++;
-        delay(250);
-      }
-      else if( 1340 < peak && peak < 1370){
-        Serial.println("E");
-        notes[numNotes] = 'E';
-        numNotes++;
-        delay(250);
-      }
-      else if(1440 < peak && peak < 1460){
-        Serial.println("F");
-        notes[numNotes] = 'F';
-        numNotes++;
-        delay(250);
-      }
-      else if(1620 < peak && peak < 1630){
-        Serial.println("G");
-        notes[numNotes] = 'G';
-        numNotes++;
-        delay(250);
-      }
-      else if(1820 < peak && peak < 1840){
-        Serial.println("A");
-        notes[numNotes] = 'A';
-        numNotes++;
-        delay(250);
-      }
-      else if(2020 < peak && peak < 2045){
-        Serial.println("B");
-        notes[numNotes] = 'B';
-        numNotes++;
-        delay(250);
-      }
-      else if(2049 < peak && peak < 2070){
-        Serial.println("C");
-        notes[numNotes] = 'H';
-        numNotes++;
-        delay(250);
-       }
-      else;
+      
     }
-    //delay(500);
-    //buttonState = digitalRead(buttonPin);
-    //if(numNotes > 5)buttonState = HIGH;
+    
+    buttonState = digitalRead(buttonPin);
     }
   Serial.println("Playing Back");
-  for(int i =0;i<180;i++){
+  for(int i =0;i<numNotes;i++){
     Serial.println(notes[i]);
+    if(i!= 0)delay(notetime[i]-breaktime);
     playNote(notes[i]);
   }
+  memset(notes,0,sizeof(notes));
+  memset(notetime,0,sizeof(notetime));
+  numNotes = 0;
+  recent = 0;
   Serial.println("End Play Back");
    
   }
@@ -203,24 +244,24 @@ void loop()
 
 
 void lowerLeft(){
-  for (leftPos = leftArm.read(); leftPos <= 30; leftPos += 1) { 
+  for (leftPos = leftArm.read(); leftPos <= 48; leftPos += 6) { 
     // in steps of 1 degree
     leftArm.write(leftPos);             
-    delay(15);                       
+    delay(35);                       
   }
-  for (leftPos = leftArm.read(); leftPos > 0; leftPos -= 1) { 
+  for (leftPos = leftArm.read(); leftPos > 0; leftPos -= 6) { 
     leftArm.write(leftPos);             
-    delay(15);                       
+    delay(35);                       
   }
 }
 void lowerRight(){
-  for (rightPos = rightArm.read(); rightPos <= 30; rightPos += 1) { 
+  for (rightPos = rightArm.read(); rightPos >= 135; rightPos -= 6) { 
     // in steps of 1 degree
     rightArm.write(rightPos);             
-    delay(15);                       
+    delay(35);                       
   }
-  for (rightPos = rightArm.read(); rightPos > 0; rightPos -= 1) { 
+  for (rightPos = rightArm.read(); rightPos < 180; rightPos += 6) { 
     rightArm.write(rightPos);             
-    delay(15);                       
+    delay(35);                       
   }
 }
